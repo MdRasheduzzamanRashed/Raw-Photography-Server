d2ee7f9f4d4f140db031fe1bc3338591a05aef31
Md Rasheduzzaman Rashed <raseduzzaman15-7495@diu.edu.bd>
2022-11-09 01:22:25 +0600

    mongodb setup data load

---
+++ index.js
@@ -0,0 +1,126 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2shkdfm.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+async function run() {
+  try {
+    const packageCollection = client
+      .db("rawPhotography")
+      .collection("packages");
+    const bookingCollection = client
+      .db("rawPhotography")
+      .collection("bookings");
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ token });
+    });
+
+    app.get("/packages", async (req, res) => {
+      const query = {};
+      const cursor = packageCollection.find(query);
+      const packages = await cursor.toArray();
+      res.send(packages);
+    });
+
+    app.get("/packages/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const package = await packageCollection.findOne(query);
+      res.send(package);
+    });
+
+    // bookings api
+    app.get("/bookings", verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+
+      if (decoded.email !== req.query.email) {
+        res.status(403).send({ message: "unauthorized access" });
+      }
+
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = bookingCollection.find(query);
+      const bookings = await cursor.toArray();
+      res.send(bookings);
+    });
+
+    app.post("/bookings", verifyJWT, async (req, res) => {
+      const order = req.body;
+      const result = await bookingCollection.insertOne(order);
+      res.send(result);
+    });
+
+    app.patch("/bookings/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const query = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = await bookingCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete("/bookings/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Raw Photography server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Raw Photography server running on ${port}`);
+});
